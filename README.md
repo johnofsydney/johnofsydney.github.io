d714a901e25c8da2b3601453da63bf5fbf35a66f
John Coote <john_coote@rocketmail.com>
2018-09-05 15:25:12 +1000

    materialize-publish

--- README.md
+++
@@ -1,5 +0,0 @@
-# John Coote
-## Web Developer
-
-This is the portfolio site for John Coote.
-This page is written with HTML, CSS and Javascript.
--- js/main.js
+++ js/main.js
@@ -1,68 +1,19 @@
-console.log("connected");
-let state = {
-  styleCounter: 0
-};
+
+M.AutoInit();
+$('.sidenav').sidenav();
+$('.carousel').carousel(
+  {
+    dist: 0,
+    padding: 0,
+    fullWidth: true,
+    indicators: true,
+    duration: 100,
+  }
+);
 
 
-
-(function($) {
-    $.fn.goTo = function() {
-        $('html, body').animate({
-            scrollTop: $(this).offset().top + 'px'
-        }, 'slow');
-        return this; // for chaining...
-    }
-})(jQuery);
-// from https://stackoverflow.com/questions/4801655/how-to-go-to-a-specific-element-on-page
-
-
-$(document).on('ready', function () {
-  console.log("ready");
-
-
-
-
-  $('.divButtContact').on('click', function () {
-    $('#contact').goTo();
-    $('.menuContent').removeClass( "show" );
-  })
-  $('.divButtPortfolio').on('click', function () {
-    $('#portfolio').goTo();
-    $('.menuContent').removeClass( "show" );
-  })
-  $('.divButtSummary').on('click', function () {
-    $('#summary').goTo();
-    $('.menuContent').removeClass( "show" );
-  })
-  $('.divButtWelcome').on('click', function () {
-    $('#welcome').goTo();
-    $('.menuContent').removeClass( "show" );
-  })
-  $('.divButtStyle').on('click', function () {
-    let styles = ["css/style.css", "css/freshStyle.css", "css/darkStyle.css"]
-
-
-    state.styleCounter = state.styleCounter + 1;
-    if (state.styleCounter >= styles.length) {
-      state.styleCounter = 0
-    }
-
-    var link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.type = "text/css";
-    link.href = styles[state.styleCounter];
-
-    $('head').append(link)
-    console.log(state.styleCounter);
-    console.log(link.href);
-
-    $('.menuContent').removeClass( "show" );
-  })
-
-  $('#divButtMenu').on('click', function () {
-    console.log("menu click");
-    $('.menuContent').toggleClass( "show" );
-  })
-
-
-})
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 7500);
+}
+autoplay()
